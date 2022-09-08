e6b9060b63fe9c33be2ecb81bd989db9914345de
Marcelo Braga <b3darcademy@outlook.com>
2022-09-08 14:55:23 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-
-
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
-  return(
-    <h1>HELLO!</h1>
+  return (
+    <>
+      <h1>HELLO!</h1>
+      <NotificationButton />
+    </>
   );
 }
 
 export default App;
+
+// As tags <></> são chamadas fragment. Isso é necessário porque o componente react não pode exportar mais de uma tag.
+// Quando tem mais de uma tag, coloca-se os componentes dentro do fragment.
